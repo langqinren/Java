4f920c3595b195e4c06d80b9038a7d4758e569e4
Shawn Liu <langqinren@hotmail.com>
2014-11-14 21:54:46 -0500

    code

---
+++ ThinkInJava/src/main/java/CollectionOfObjects/SimpleHashMap.java
@@ -0,0 +1,17 @@
+package CollectionOfObjects;
+
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by Shawn on 11/14/2014.
+ */
+public class SimpleHashMap<K,V>  extends AbstractMap<K,V> {
+    
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        return null;
+    }
+}
